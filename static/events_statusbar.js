7e6d170bdaf902ba269cc3745d0851ff34c2113e
ovolosch <ovolosch@cisco.com>
2015-06-15 11:37:44 +0300

    Resolved status websocket  connection issue.

--- static/events_statusbar.js
+++ static/events_statusbar.js
@@ -25,19 +25,26 @@ define(function(require){
         }
     });
 
-    var wsStatus = new WebSocket('ws://' + window.location.host + '/status.ws');
-    wsStatus.onmessage = function(evt) {
-        var data = evt.data;
-        data = JSON.parse(data);
-        switch (data.action) {
-            case 'LIST':
-                tasksReceivedAction(data.tasks);
-                break;
-            case 'UPDATE':
-                taskUpdatedAction(data.task);
-                break;
+    var connect = function() {
+        var wsStatus = new WebSocket('ws://' + window.location.host + '/status.ws');
+        wsStatus.onmessage = function (evt) {
+            var data = evt.data;
+            data = JSON.parse(data);
+            switch (data.action) {
+                case 'LIST':
+                    tasksReceivedAction(data.tasks);
+                    break;
+                case 'UPDATE':
+                    taskUpdatedAction(data.task);
+                    break;
+            }
+        };
+        wsStatus.onclose = function () {
+            setTimeout(connect, 1000);
         }
     };
+
+    connect();
 
     return {
         actions: {
